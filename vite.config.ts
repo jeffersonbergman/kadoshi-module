81e63cf7e43e256d4cff61f6427bd8ad2f44dbce
jeffersonbergman <jefferson.bergman@yahoo.com>
2025-04-26 21:20:20 +0100

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kadoshi-module/',
+  base: mode === 'production' ? '/kadoshi-module/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
